40a137cbb8ac9c624e81b2917e1163a58fc13bd5
Kristina Capova <74kricap@skola.boras.se>
2021-08-20 11:59:37 +0200

    added a item and some code to get by id

--- server.js
+++ server.js
@@ -2,18 +2,28 @@ const express = require('express');
 const app = express();
 
 //In-memory DB
-const cosmetics = [];
+const cosmeticIdIndex = 1;
+const cosmetics = [{
+    id: 0,
+    name: 'Lipstick',
+    color: 'Red'
+
+}];
 
 //Parse incomming JSON
 app.use(express.json());
 
 //Resource Endpoints
 app.get('/api/cosmetics', (req, res, next) => {
-    res.json([]);
+    res.json(cosmetics);
 })
 
 app.get('/api/cosmetics/:id', (req, res, next) => {
-    res.json('');
+    const {id} = req.params;
+
+    const cosmetic = cosmetics.find(cosmetic => cosmetic.id == id);
+    
+    res.json(cosmetic);
 })
 
 app.post('/api/cosmetics');
