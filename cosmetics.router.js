4512c2728cefc71c9bd5e7592dd4b75ac1bb4e36
Kristina Capova <74kricap@skola.boras.se>
2021-08-24 12:29:59 +0200

    Added validation and save function to post request

--- cosmetics.router.js
+++ cosmetics.router.js
@@ -1,16 +1,20 @@
 const express = require('express');
 const {
     getCosmetics, 
-    getOneCosmetic} 
+    getOneCosmetic,
+    saveCosmetic} 
     = require('./cosmetics.controllers');
+const { saveValidation } = require('./cosmetics.validation');
 
 //Create the router object
 const router = express.Router();
 
+
+
 //Define endpoints
 router.get('/api/cosmetics', getCosmetics);
 router.get('/api/cosmetics/:id', getOneCosmetic);
-router.post('/api/cosmetics');
+router.post('/api/cosmetics', saveValidation, saveCosmetic);
 router.put('/api/cosmetics/:id');
 router.delete('/api/cosmetics/:id');
 
