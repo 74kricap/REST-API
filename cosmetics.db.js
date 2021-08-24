3b4d03192ea18b77f4923675005d432af58531ab
Kristina Capova <74kricap@skola.boras.se>
2021-08-24 22:55:06 +0200

    Moved db data to separate file

---
+++ cosmetics.db.js
@@ -0,0 +1,17 @@
+//In-memory DB
+const cosmetics = [{
+    id: 1,
+    name: 'Lipstick',
+    color: 'Red',
+    price: '199kr'
+},
+{
+    id: 2,
+    name: 'Foundation',
+    color: 'Fairly Light',
+    price: '399kr'
+}];
+
+module.exports = {
+    cosmetics
+}
