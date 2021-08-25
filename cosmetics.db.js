99292e417ca330ecb83ecb85fb030d9a82b2432f
Kristina Capova <74kricap@skola.boras.se>
2021-08-25 20:32:40 +0200

    Added a new property

--- cosmetics.db.js
+++ cosmetics.db.js
@@ -1,15 +1,17 @@
 //In-memory DB
 const cosmetics = [{
     id: "5968d290-05c9-11ec-824b-15fd2ebb3405",
+    brand: 'Estée Lauder',
     name: 'Lipstick',
     color: 'Red',
-    price: '199kr'
+    price: '349kr'
 },
 {
     id: "19e9f3c0-05cd-11ec-824b-15fd2ebb3405",
+    brand: 'La Prairie',
     name: 'Foundation',
     color: 'Fairly Light',
-    price: '399kr'
+    price: '2399kr'
 }];
 
 module.exports = {
