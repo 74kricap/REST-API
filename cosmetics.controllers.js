810b63c3f82f552801b08292e01aa2a7fef25857
Kristina Capova <74kricap@skola.boras.se>
2021-08-28 23:52:07 +0200

    Added read/write file to deleteCosmetic function

--- cosmetics.controllers.js
+++ cosmetics.controllers.js
@@ -3,6 +3,7 @@ const { Request, Response, NextFunction } = require('express');
 const { saveValidation } = require('./cosmetics.validation');
 const { cosmetics } = require('./data/cosmetics.db.json');
 const fs = require('fs');
+const { isContext } = require('vm');
 const dataPath = './data/cosmetics.db.json';
 
 /**
@@ -114,14 +115,30 @@ const updateCosmetic = (req, res, next) => {
  * @param {NextFunction} next 
  */
 const deleteCosmetic = (req, res, next) => {
-
     const { id } = req.params;
-    const index = cosmetics.findIndex(cosmetic => cosmetic.id == id);
-    if (!index) {
-        res.status(404).json(`Cosmetic with id ${id} was not found!`);
-    }
-    cosmetics.splice(index, 1);
-    res.json(cosmetics);
+    const cosmetic = { ...req.body };
+    fs.readFile(dataPath, 'utf8', (err, data) => {
+        if (err) {
+            res.status(404).json('No cosmetics found!');
+            return
+        }
+        let newCosmetic = JSON.parse(data);
+        const deleteCosmetic = newCosmetic.find(cosmetic => cosmetic.id == id);
+        if (deleteCosmetic) {
+            newCosmetic = newCosmetic.filter(cosmetic => cosmetic.id !== id);
+            res.status(200).send(`Cosmetic with id:${id} removed`);
+        } else {
+            res.status(404).json(`Cosmetic with id ${id} was not found!`);
+        }
+
+        fs.writeFile(dataPath, JSON.stringify(newCosmetic, null, 2), () => {
+            if (err) {
+                res.status(500).json('Someting went wrong!');
+                return
+            }
+            res.status(200).json(cosmetic);
+        });
+    });
 }
 
 module.exports = {
